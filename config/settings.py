b9ab353004fd4db597d42b767c66dcd19d4b5afc
Elvin Ronal Cardenas <ecardenc@emeal.nttdata.com>
2023-01-08 23:16:13 -0500

    ojsls funke

--- config/settings.py
+++ config/settings.py
@@ -12,42 +12,43 @@ USE_I18N = True
 USE_L10N = True
 
 DATABASES = {
-	'default': {
-		'NAME': "os.path.join(BASE_DIR, 'db.sqlite3')",
-		'ENGINE': "django.db.backends.sqlite3",
-	}
+    'default': {
+        'NAME': "os.path.join(BASE_DIR, 'db.sqlite3')",
+        'ENGINE': "django.db.backends.sqlite3",
+    }
 }
 
 TEMPLATES = [
-	{
-		'BACKEND': 'django.template.backends.django.DjangoTemplates',
-		'DIRS': [],
-		'APP_DIRS': True,
-		'OPTIONS': {
-			'context_processors': [
-				'django.template.context_processors.debug',
-				'django.template.context_processors.request',
-				'django.contrib.auth.context_processors.auth',
-				'django.contrib.messages.context_processors.messages',
-			]
-		}
-	}
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ]
+        }
+    }
 ]
 
 TIME_ZONE = 'UTC'
 
 MIDDLEWARE = [
-	'django.middleware.security.SecurityMiddleware',
-	'django.contrib.sessions.middleware.SessionMiddleware',
-	'corsheaders.middleware.CorsMiddleware',
-	'django.middleware.common.CommonMiddleware',
-	'django.middleware.csrf.CsrfViewMiddleware',
-	'django.contrib.auth.middleware.AuthenticationMiddleware',
-	'django.contrib.messages.middleware.MessageMiddleware',
-	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-SECRET_KEY = 's_b=5%2n=!(dehix7vlv*r3*si)+kjob3ev=6k%kknv%sd#hk2'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 STATIC_URL = '/static/'
 
@@ -55,46 +56,50 @@ ROOT_URLCONF = 'config.urls'
 
 ALLOWED_HOSTS = ['*']
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+	ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 LANGUAGE_CODE = 'en-us'
 
 
 INSTALLED_APPS = [
-	'django.contrib.admin',
-	'django.contrib.auth',
-	'django.contrib.contenttypes',
-	'django.contrib.sessions',
-	'django.contrib.messages',
-	'django.contrib.staticfiles',
-	'corsheaders',
-	'rest_framework',
-	'portfolio.core',
-	'portfolio.commons',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'portfolio.core',
+    'portfolio.commons',
 
-	'cloudinary_storage',
+    'cloudinary_storage',
     'cloudinary',
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
 CORS_ALLOW_HEADERS = [
-	'x-requested-with',
-	'content-type',
-	'accept',
-	'origin',
-	'authorization',
-	'accept-encoding',
-	'x-csrftoken',
-	'access-control-allow-origin',
-	'content-disposition',
+    'x-requested-with',
+    'content-type',
+    'accept',
+    'origin',
+    'authorization',
+    'accept-encoding',
+    'x-csrftoken',
+    'access-control-allow-origin',
+    'content-disposition',
 ]
 
 CORS_ALLOW_METHODS = [
-	'GET',
-	'POST',
-	'PUT',
-	'PATCH',
-	'DELETE',
-	'OPTIONS',
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
