1903ebb65b187e5461bc3858578eeaa8181e5448
Elvin Ronal Cardenas <ecardenc@emeal.nttdata.com>
2023-01-14 01:16:27 -0500

    se puso las migraciones en el build.sh

--- render/build.sh
+++ render/build.sh
@@ -6,4 +6,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-# python manage.py migrate
+
+python manage.py makemigrations 
+
+python manage.py migrate
